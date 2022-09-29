3065f15ddc271cecab4fa5b1930a0d070d7a3171
Tim Lebrun <tim.lebrun@airweb.fr>
2022-09-29 22:17:26 +0200

    :sparkles: Setting up Edge templating engine

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { WsAdapter } from '@nestjs/platform-ws';
+
+import { Edge, EdgeOptions } from 'edge.js';
+import { join } from 'path';
 
 import { Hatcher } from './hatcher';
 
 (async function() {
-	const app = await NestFactory.create(Hatcher);
+	const app = await NestFactory.create<NestExpressApplication>(Hatcher);
 	app.useWebSocketAdapter(new WsAdapter(app));
+
+	app.engine('edge', edgeEngine());
+
+	app.useStaticAssets(join(__dirname, '..', 'public'));
+	app.setBaseViewsDir(join(__dirname, '..', 'views'));
+	app.setViewEngine('edge');
 
 	await app.listen(3000);
 })();
+
+function edgeEngine(options: EdgeOptions = {}) {
+	const edge = new Edge(options);
+
+	return function (filePath: string, data: any, next) {
+		edge.mount(data.settings.views); // TODO: find a way to do only on startup
+
+		edge
+			.render(filePath, data)
+			.then(html => next(null, html))
+			.catch(error => next(error, null));
+	};
+}
