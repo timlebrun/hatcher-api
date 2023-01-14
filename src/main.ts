c5cd6f862a4fe1279789050e7eeeb2d167c017d5
Tim Lebrun <tim.lebrun@airweb.fr>
2023-01-14 15:36:01 +0100

    :art: Improving Edge as template engine

--- src/main.ts
+++ src/main.ts
@@ -10,22 +10,24 @@ import { Hatcher } from './hatcher';
 
 (async function() {
 	const app = await NestFactory.create<NestExpressApplication>(Hatcher);
-	app.useWebSocketAdapter(new WsAdapter(app));
 
-	app.engine('edge', edgeEngine());
+	app.useWebSocketAdapter(new WsAdapter(app));
 
 	app.useStaticAssets(join(__dirname, '..', 'public'));
 	app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+	const edge = new Edge({});
+	app.engine('edge', makeEdgeEngineAdapter({ edge }));
 	app.setViewEngine('edge');
 
-	await app.listen(3000);
+	await app.listen(8000);
 })();
 
-function edgeEngine(options: EdgeOptions = {}) {
-	const edge = new Edge(options);
+function makeEdgeEngineAdapter(options: EdgeOptions & { edge?: Edge }) {
+	const edge = options.edge || new Edge(options);
 
-	return function (filePath: string, data: any, next) {
-		edge.mount(data.settings.views); // TODO: find a way to do only on startup
+	return function(filePath: string, data: any, next) {
+		if (data.settings?.views) edge.mount('default', data.settings?.views); // NOTE: required to use express configuration
 
 		edge
 			.render(filePath, data)
