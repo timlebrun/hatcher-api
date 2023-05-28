37063832986e4e5ff7c9e6e6ebadbd8c3c64bf83
Tim Lebrun <tim.lebrun@airweb.fr>
2023-05-28 20:02:56 +0200

    :sparkles: Adding a first draft of chat channel page

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,6 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { HatcherSoundService } from 'src/modules/sound/sound.service';
 
+/**
+ * This controller centralizes all pages exposed by the API
+ * 
+ * The page definition is separate from the corresponding modules
+ * because the goal of this API is to provide a server, pages are
+ * mainly for testing purposes
+ */
 @Controller('pages')
 export class HatcherPageController {
 	constructor(private readonly sound: HatcherSoundService) {}
@@ -14,6 +21,12 @@ export class HatcherPageController {
 	@Get('sound/listener')
 	@Render('pages/sound/listener')
 	public async getSoundListenerPage() {
+		return {};
+	}
+
+	@Get('tchat/channel')
+	@Render('pages/tchat/channel')
+	public async getTchatChannelPage() {
 		return {};
 	}
 }
